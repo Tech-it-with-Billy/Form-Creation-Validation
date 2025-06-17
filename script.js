afa7036896882eac6fb1ff05a6cb7a971a694dab
Billy Och <ochiengbilly02@gmail.com>
2025-06-17 20:53:31 +0300

    update files

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Displaying Feedback
         feedbackDiv.style.display = 'block'
         if (isValid) {
-            feedbackDiv.textContent = 'Registration Successful!';
+            feedbackDiv.textContent = 'Registration successful!';
             feedbackDiv.style.color = '#28a745';
         } else {
             feedbackDiv.innerHTML = messages.join('<br>');
